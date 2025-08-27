0b588708aa82dc72ee42e28be62ccf07a79636f8
nejikot <nejikot@yandex.ru>
2025-08-27 23:39:40 +0300

    modified:   backend/app.py

--- backend/app.py
+++ backend/app.py
@@ -15,7 +15,12 @@ DATABASE_URL = os.getenv("DATABASE_URL")
 if not DATABASE_URL:
     raise RuntimeError("DATABASE_URL env var required")
 
-engine = create_engine(DATABASE_URL, echo=False, future=True)
+engine = create_engine(
+    DATABASE_URL,
+    echo=False,
+    future=True,
+    connect_args={"sslmode": "require"}
+)
 SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
 Base = declarative_base()
 
